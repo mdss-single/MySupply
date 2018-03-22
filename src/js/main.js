dd1b2b04d3ff300327d412f40ab30a4dbafe5692
mdss@makexhtml.ru <mdss@makexhtml.ru>
2018-03-22 21:27:46 +0300

    предварительная версия карточки товара

--- src/js/main.js
+++ src/js/main.js
@@ -76,6 +76,34 @@
 		}
 	});
 
+	// product offer map
+	ymaps.ready(init);
+	function init () {
+		var myMap = new ymaps.Map('productOffersMap', {
+			center: [55.76, 37.64],
+			zoom: 10
+		}, {
+			searchControlProvider: 'yandex#search'
+		}),
+		objectManager = new ymaps.ObjectManager({
+			// Чтобы метки начали кластеризоваться, выставляем опцию.
+			clusterize: true,
+			// ObjectManager принимает те же опции, что и кластеризатор.
+			gridSize: 32,
+			clusterDisableClickZoom: true
+		});
+		// Чтобы задать опции одиночным объектам и кластерам,
+		// обратимся к дочерним коллекциям ObjectManager.
+		objectManager.objects.options.set('preset', 'islands#greenDotIcon');
+		objectManager.clusters.options.set('preset', 'islands#greenClusterIcons');
+		myMap.geoObjects.add(objectManager);
+		$.ajax({
+			url: "js/data.json"
+		}).done(function(data) {
+			objectManager.add(data);
+		});
+	}
+
 	function inputWidth(elem, minW, maxW) {
 		elem = $(this);
 	}
