c07076481f337895afeb99c91bf28fcfd045c23f
mdss@makexhtml.ru <mdss@makexhtml.ru>
2018-03-26 15:47:57 +0300

    cabinet discount

--- src/js/libs/spinner.js
+++ src/js/libs/spinner.js
@@ -5,13 +5,15 @@
 			el.after('<a href="#" class="category-product__toCart-minus js-category-product-spinner-minus" role="button"></a>');
 			el.parent().on('click', '.js-category-product-spinner-minus', function (e) {
 				e.preventDefault();
-				if (el.val() > parseInt(el.attr('min')))
+				if (el.val() > parseInt(el.attr('min'))) {
 					el.val( function(i, oldval) { return --oldval; });
+				}
 			});
 			el.parent().on('click', '.js-category-product-spinner-plus', function (e) {
 				e.preventDefault();
-				if (el.val() < parseInt(el.attr('max')))
+				if (el.val() < parseInt(el.attr('max'))) {
 					el.val( function(i, oldval) { return ++oldval; });
+				}
 			});
 			el.on('focusout', function() {
 				if (el.val() > parseInt(el.attr('max'))) {
